d41aa19d493320813cc9361bf940313b9256b152
Enobakhare Princely <enobakhareprincely@gmail.com>
2021-07-13 20:05:37 +0100

    install vue

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+ <div class="container">
+    <div class="card bg-light shadow text-dark">
+      <div class="card-body text-center">
+        <h3 class="card-title">
+          ToDo App
+        </h3>
+
+        .card-text
+      </div>
+    </div>
+</div>
+</template>
+
+<script>
+
+
+export default {
+  name: 'App',
+  components: {
+  }
+}
+</script>
+
+<style>
+
+</style>
